66cb7780290e2bf45f5060f720546c80affa34e4
EzequielMenendez <ezemenendez2003@gmail.com>
2023-07-31 16:32:14 -0300

    init(master): add server and client/Ezequiel

--- server/db.js
+++ server/db.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
