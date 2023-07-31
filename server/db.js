7938b9ef2332376415a183ad5782b7e483306f11
EzequielMenendez <ezemenendez2003@gmail.com>
2023-07-31 17:46:14 -0300

    feat(server): creacion basica del server/Ezequiel

--- server/db.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
-const fs = require('fs');
-const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
-
---
+++ server/src/db.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
+const { DB_DEPLOY } = process.env;
+
+const sequelize = new Sequelize(
+    DB_DEPLOY,
+    {
+       logging: false, 
+       native: false, 
+    }
+ );
+ const basename = path.basename(__filename);
+
+const modelDefiners = [];
+
+fs.readdirSync(path.join(__dirname, '/models'))
+   .filter(
+      (file) =>
+         file.indexOf('.') !== 0 &&
+         file !== basename &&
+         file.slice(-3) === '.js'
+   )
+   .forEach((file) => {
+      modelDefiners.push(require(path.join(__dirname, '/models', file)));
+});
+
+modelDefiners.forEach((model) => model(sequelize));
+let entries = Object.entries(sequelize.models);
+let capsEntries = entries.map((entry) => [
+   entry[0][0].toUpperCase() + entry[0].slice(1),
+   entry[1],
+]);
+sequelize.models = Object.fromEntries(capsEntries);
+
+// En sequelize.models están todos los modelos importados como propiedades
+// Para relacionarlos hacemos un destructuring
+// Aca vendrian las relaciones
+
+module.exports = {
+   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
+   conn: sequelize, // para importart la conexión { conn } = require('./db.js');
+};
+
