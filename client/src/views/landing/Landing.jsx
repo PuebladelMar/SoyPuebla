a40ed33f9a4463ad8f1a5a4eb1f41e6e3e3d2582
Agustin <106754820+AgusNazer@users.noreply.github.com>
2023-08-01 15:54:55 -0300

    feat(client/landing):add landingPage - A. nazer (#8)

---
+++ client/src/views/landing/Landing.jsx
@@ -0,0 +1,18 @@
+import landing from '../../assets/landing.gif'
+import style from '../landing/Landig.css'
+import { NavLink } from 'react-router-dom';
+
+function Landing() {
+    return (
+      <div>
+         <div>
+           <video src={landing} className={style.landing} autoPlay loop muted></video>
+         </div>
+          <NavLink to='/home'>
+            <button className={style.home}>Ingresar</button>
+          </NavLink>
+      </div>
+    );
+  }
+  
+  export default Landing;
