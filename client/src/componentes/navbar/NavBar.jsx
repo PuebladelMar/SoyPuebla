1306366941b0029ac1d0e7e64e734a85fc7f5ab4
Juan Cruz Mambretti <juancmam7@gmail.com>
2023-08-01 15:33:46 -0300

    feat(client/nav-bar): add nav-bar - J.Cruz

---
+++ client/src/componentes/navbar/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from React;
+import './NavBar.css'
+
+
+const NavBar = () => {
+    return(
+        <div className='navbar'> 
+            <form 
+                onSubmit={''}
+            />
+
+            <input
+                className='search-input'
+                type='text'
+                placeholder="Buscar"
+                onChange={''}
+            />
+            <button className='navbar-button'>Buscar</button>
+
+        </div>
+    )
+}
+
+export default NavBar
