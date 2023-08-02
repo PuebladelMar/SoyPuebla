017ee81b52bded15783657c01c622c5b38a45ee6
EzequielMenendez <ezemenendez2003@gmail.com>
2023-08-02 09:54:19 -0300

    feat(handler-controller/postSize):added postSize routes/Ezequiel

--- server/src/handlers/Products/postSize.js
+++ server/src/handlers/Products/postSize.js
@@ -1,7 +1,13 @@
-
+const controllPostSize = require("../../controllers/Products/controllPostSize");
 
 const postSize = async(req,res)=>{
-
-}
+    try {
+        const { size } = req.body;
+        const newSize = await controllPostSize(size);
+        return res.status(201).json(newSize);
+    } catch (error) {
+        res.status(500).json(error.message);
+    };
+};
 
 module.exports = postSize;
