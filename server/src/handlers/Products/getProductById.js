774fb9255481ec03c1c8785f2d8d7b487b656f1a
Matias Graner <matiasgraner@MacBook-Pro-de-Matias.local>
2023-08-02 16:53:28 -0300

    feat(server/src/controllers/Products/controllePostById):create controller and handlers /Matias

--- server/src/handlers/Products/getProductById.js
+++ server/src/handlers/Products/getProductById.js
@@ -1,7 +1,17 @@
+const getProductsById = async (req, res) => {
+  const { id } = req.params;
 
+  try {
+    const response = await productById(id);
 
-const getProductsById = async(req,res)=>{
-
-}
+    if (!response) {
+      res.status(400).send("non-existent product");
+    } else {
+      res.status(201).json(response);
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 module.exports = getProductsById;
