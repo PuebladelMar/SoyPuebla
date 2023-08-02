b5beee16ac9aa03e198908077c6cd799b76be1f7
MatuUrrutia <matu.urrutia.dt@gmail.com>
2023-08-02 00:45:06 -0400

    feat (client/cardContainer): Create card container - M. Urrutia

---
+++ client/src/componentes/cardContainer/CardContainer.jsx
@@ -0,0 +1,24 @@
+import Card from "../card/Card";
+
+import "./CardContainer.css";
+
+
+function CardContainer() {
+  
+
+  return (
+    <div className="card-list">
+      
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+      
+    </div>
+  );
+}
+
+
+export default CardContainer;
