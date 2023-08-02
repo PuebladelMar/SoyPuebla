7265f9ac1d160480c93348269a61e124ddd95381
CaritoCarvacho <flga.carolinacarvacho@gmail.com>
2023-08-02 00:44:28 -0300

    feat(client/componentes/footer):add footer.jsx/css - Carito

--- client/src/componentes/footer/Footer.jsx
+++ client/src/componentes/footer/Footer.jsx
@@ -6,7 +6,6 @@ const Footer = () => {
     <footer>
       <div className="footer-container">
         <p>© 2023 Puebla del Mar. Todos los derechos reservados.</p>
-        <p>DUEÑAS?</p>
 
         <div className="social-icons">
           <a
@@ -14,22 +13,38 @@ const Footer = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src="/assets/images/Whatsapp.png" alt="WhatsApp" />
+            <img src="src/assets/images/Whatsapp.png" alt="WhatsApp" />
           </a>
           <a
             href="https://www.facebook.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src="assets/images/Whatsapp.png" alt="Facebook" />
+            <img src="src/assets/images/face.png" alt="Facebook" />
           </a>
           <a
             href="https://www.instagram.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src="/assets/images/Insta.png" alt="Instagram" />
+            <img src="src/assets/images/Insta.png" alt="Instagram" />
           </a>
+        </div>
+
+        <div className="footer-links">
+          <a href="/centro-ayuda">Centro de Ayuda</a>
+          <a href="/preguntas-frecuentes">Preguntas Frecuentes</a>
+          <a href="/suscripcion">Suscripción al Newsletter</a>
+          <a href="/centro-ayuda">Quienes somos</a>
+          <a href="/centro-ayuda">Cambios y Devoluciones</a>
+          <a href="/centro-ayuda">Medios de pago</a>
+          <a href="/centro-ayuda">Medios de envío</a>
+        </div>
+
+        <div className="brand-info">
+          <h3>Contactanos</h3>
+          <p>Email: info@xxxxxxxx.com</p>
+          <p>Teléfono: +1 234 567 890</p>
         </div>
       </div>
     </footer>
