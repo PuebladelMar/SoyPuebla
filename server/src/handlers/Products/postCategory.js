9426594069a637c84ce51836f7e809e9edb3b330
Matias Graner <matiasgraner@MacBook-Pro-de-Matias.local>
2023-08-02 10:48:12 -0300

    feat(server/src/handlers/Products/postCategory):create handler category/Matias

--- server/src/handlers/Products/postCategory.js
+++ server/src/handlers/Products/postCategory.js
@@ -1,7 +1,16 @@
+const {createPostCategoryDB} =require("../../controllers/Products/controllPostCategory")
+
 
 
 const postCategory = async(req,res)=>{
-
+    
+    try {
+      const {category} = req.body
+     const response= await createPostCategoryDB(category)
+     res.status(200).json(response) 
+   } catch (error) {
+      res.status(400).send(error.message)
+    }
 }
 
 module.exports = postCategory;
