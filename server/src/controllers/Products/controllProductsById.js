da0ad79b5601c602639ff8b5fffe00de652ab10e
Matias Graner <matiasgraner@MacBook-Pro-de-Matias.local>
2023-08-03 14:51:18 -0300

    feat(controllProductById-GetById):add routes

--- server/src/controllers/Products/controllProductsById.js
+++ server/src/controllers/Products/controllProductsById.js
@@ -1,4 +1,4 @@
-const { Products, Colors, Sizes, Categories, Series, Stocks } = require("../../db");
+const {Products,Colors,Sizes,Categories,Series,Stocks} = require("../../db");
 
 const productById = async (id) => {
   let product = await Products.findByPk(id, {
@@ -20,38 +20,41 @@ const productById = async (id) => {
     ],
   });
 
-  let ubicationColor = product.Stocks[0].ColorId;
-  let ubicationSize = product.Stocks[0].SizeId;
+  let concatenatedResults = [];
 
-  let color = await Colors.findOne({
-    where: {
-      id: ubicationColor,
-    },
-  });
+  for (const stock of product.Stocks) {
+    let color = await Colors.findOne({
+      where: {
+        id: stock.ColorId,
+      },
+    });
 
-  let size = await Sizes.findOne({
-    where: {
-      id: ubicationSize,
-    },
-  });
+    let size = await Sizes.findOne({
+      where: {
+        id: stock.SizeId,
+      },
+    });
 
-  let concatenatedResult = {
-    id: product.id,
-    name: product.name,
-    price: product.price,
-    mainImage: product.mainImage,
-    image: product.image,
-    description: product.description,
-    sale: product.sale,
-    category: product.Categories,
-    series: product.Series,
-    stock: product.Stocks[0].amount,
-    color: color.name,
-    codHex: color.codHex,
-    size: size.name,
-  };
+    let concatenatedResult = {
+      id: product.id,
+      name: product.name,
+      price: product.price,
+      mainImage: product.mainImage,
+      image: product.image,
+      description: product.description,
+      sale: product.sale,
+      category: product.Categories,
+      series: product.Series,
+      stock: stock.amount,
+      color: color.name,
+      codHex: color.codHex,
+      size: size.name,
+    };
 
-  return concatenatedResult;
+    concatenatedResults.push(concatenatedResult);
+  }
+
+  return concatenatedResults;
 };
 
 module.exports = { productById };
