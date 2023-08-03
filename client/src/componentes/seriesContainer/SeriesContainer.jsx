b2989b8559a15efb56bfc902707f27b295dcd9ea
Matu Urrutia <115486717+MatuUrrutia@users.noreply.github.com>
2023-08-02 21:09:12 -0400

    feat (client/seriesContainer): Create series files - M. Urrutia (#51)

---
+++ client/src/componentes/seriesContainer/SeriesContainer.jsx
@@ -0,0 +1,98 @@
+import "./SeriesContainer.css";
+// import R1  from '../../assets/images/imagesSeries/R1 INDUSTRIA.png'
+// import R2  from '../../assets/images/imagesSeries/R2 ESCAPE.png'
+// import R3  from '../../assets/images/imagesSeries/R3 LIGHT.png'
+// import R4  from '../../assets/images/imagesSeries/R4 SCRAPES.png'
+// import R5  from '../../assets/images/imagesSeries/R5 BEACH.png'
+// import R6  from '../../assets/images/imagesSeries/R6 CLEAR.png'
+// import R7  from '../../assets/images/imagesSeries/R7 FORCE.png'
+// import R8  from '../../assets/images/imagesSeries/R8 PURE.png'
+// import R9  from '../../assets/images/imagesSeries/R9 URBAN.png'
+// import R10  from '../../assets/images/imagesSeries/R10 ZEN.png'
+
+function SeriesContainer() {
+  
+  return (
+    <div className="seriesContainer">
+
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis officia quisquam dignissimos nihil quos temporibus perspiciatis odit minima animi eveniet!</div>
+            <div>Voluptatum ratione sint ducimus quam enim, similique repellendus earum numquam culpa cumque inventore maxime iure pariatur omnis vel impedit quia.</div>
+            <div>Obcaecati, aperiam voluptatibus ipsa possimus sapiente eligendi non commodi culpa est quae perspiciatis optio, ad at minima maiores, ab voluptatum.</div>
+            <div>Sit veniam asperiores accusamus doloribus, nesciunt beatae assumenda nihil quisquam maxime itaque quibusdam, debitis similique voluptatibus aperiam, voluptatum adipisci modi!</div>
+            <div>Ex repudiandae similique incidunt aliquid sit hic magni, suscipit voluptatibus saepe. Aliquam, tempore voluptatem. Culpa amet fugiat quia saepe facilis?</div>
+            <div>Alias deserunt nobis, autem error sed, doloremque earum quaerat ipsa natus repellendus ex in commodi aperiam excepturi vel. Saepe, quo?</div>
+            <div>Vel tenetur architecto quo hic repellat soluta obcaecati commodi repellendus voluptatum, explicabo officiis et ipsa consectetur eius tempore magnam temporibus.</div>
+            <div>Molestiae maxime atque vel expedita, repudiandae et blanditiis doloremque iure aperiam commodi harum illo in dolorem amet dolores, optio laborum!</div>
+            <div>Sit, facilis cum nihil, veritatis adipisci maiores odit voluptate dolorem ipsum totam expedita quia quos necessitatibus! Aut est vel voluptates.</div>
+            <div>Distinctio architecto doloribus ut perspiciatis consequuntur culpa aspernatur voluptates quisquam facere aut, magni quibusdam numquam reiciendis sed nobis vero. Sequi?</div>
+      
+    </div>
+  );
+}
+
+
+export default SeriesContainer;
+
+
+   {/* <img className="serie1" src={R1} alt="INDUSTRIA" />
+            <img className="serie2"  src={R2} alt="ESCAPE" />
+            <img className="serie3"  src={R3} alt="LIGHT" />
+            <img className="serie4"  src={R4} alt="SCRAPES" />
+            <img className="serie5"  src={R5} alt="BEACH" />
+            <img className="serie6"  src={R6} alt="CLEAR" />
+            <img className="serie7"  src={R7} alt="FORCE" />
+            <img className="serie8"  src={R8} alt="PURE" />
+            <img className="serie9"  src={R9} alt="URBAN" />
+            <img className="serie10"  src={R10} alt="ZEN" /> */}
+
+
+
+// function SeriesContainer() {
+  
+//     return (
+//       <div className="seriesContainer">
+//           <div className="line">
+//               <img className="serie" src={R1} alt="INDUSTRIA" />
+//               <img className="serie"  src={R2} alt="ESCAPE" />
+//               <img className="serie"  src={R3} alt="LIGHT" />
+//           </div>
+//           <div className="line">
+//               <img className="serie"  src={R4} alt="SCRAPES" />
+//               <img className="serie"  src={R5} alt="BEACH" />
+//           </div>
+//           <div className="line">
+//               <img className="serie"  src={R6} alt="CLEAR" />
+//               <img className="serie"  src={R7} alt="FORCE" />
+//           </div>
+//           <div className="line">
+//               <img className="serie"  src={R8} alt="PURE" />
+//               <img className="serie"  src={R9} alt="URBAN" />
+//               <img className="serie"  src={R10} alt="ZEN" />
+//           </div>
+  
+//       </div>
+//     );
+//   }
+  
+  
+//   export default SeriesContainer;
+  
+
+
+
+// import React from 'react';
+// import "./SeriesContainer.css";
+
+// const imagesPath = '../../assets/images/imagesSeries/';
+
+// function CardContainer() {
+//   return (
+//     <div className="row">
+//       <div className="column">
+//         <img src={imagesPath + 'R1 INDUSTRIA.png'} alt="INDUSTRIA" />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default CardContainer;
