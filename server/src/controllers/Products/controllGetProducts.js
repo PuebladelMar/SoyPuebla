351f2d376405f4f24137531892848e19757dc289
Matias Graner <matiasgraner@MacBook-Pro-de-Matias.local>
2023-08-03 17:57:17 -0300

    feat(src/controllers/controllGetProducts): add getByName/Matias

--- server/src/controllers/Products/controllGetProducts.js
+++ server/src/controllers/Products/controllGetProducts.js
@@ -1,15 +1,30 @@
-const {
-  Products,
-  Colors,
-  Sizes,
-  Categories,
-  Series,
-  Stocks,
-} = require("../../db.js");
+const {Products,Colors,Sizes,Categories,Series,Stocks} = require("../../db.js");
+const {Op}=require("sequelize")
 
 const controllGetProducts = async (req) => {
-  const { color, size, category, serie, sale, price, order } = req.query;
+  const { color, size, category, serie, sale, price, order, name } = req.query;
+  if (name) {
+    const capitalizeString = (str) => {
+      let string = str.toString();
+      return str
+        .split(" ")
+        .map(
+          (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+        )
+        .join(" ");
+    };
+    const productWanted = capitalizeString(name).trim();
 
+    const productByName = await Products.findAll({
+      where: {
+        name: {
+          [Op.iLike]: `%${productWanted}%`, //Op.iLike de Sequelize para realizar una búsqueda insensible a mayúsculas y minúsculas.
+        },
+      },
+    });
+   
+    return productByName
+  }
   const products = await Products.findAll({
     include: [
       {
@@ -42,56 +57,58 @@ const controllGetProducts = async (req) => {
     };
   });
 
-  if(color) {
+  if (color) {
     const response = [];
     const findColor = await Colors.findOne({ where: { name: color } });
     filterProducts.map((product) => {
-      const match = product.stock.find(stock => stock.ColorId === findColor.id);
+      const match = product.stock.find(
+        (stock) => stock.ColorId === findColor.id
+      );
       if (match) {
         response.push(product);
-      };
+      }
     });
     filterProducts = response;
-  };
+  }
 
-  if(size){
+  if (size) {
     const response = [];
     const findSize = await Sizes.findOne({ where: { name: size } });
     filterProducts.map((product) => {
-      const match = product.stock.find(stock => stock.SizeId === findSize.id);
+      const match = product.stock.find((stock) => stock.SizeId === findSize.id);
       if (match) {
         response.push(product);
-      };
+      }
     });
     filterProducts = response;
-  };
+  }
 
-  if(category){
+  if (category) {
     const response = [];
     filterProducts.map((product) => {
-      const match = product.category.find(c => c.name === category);
+      const match = product.category.find((c) => c.name === category);
       if (match) {
         response.push(product);
-      };
+      }
     });
     filterProducts = response;
-  };
+  }
 
-  if(serie){
+  if (serie) {
     const response = [];
     filterProducts.map((product) => {
-      const match = product.series.find(s => s.name === serie);
+      const match = product.series.find((s) => s.name === serie);
       if (match) {
         response.push(product);
-      };
+      }
     });
     filterProducts = response;
-  };
+  }
 
-  if(sale){
+  if (sale) {
     const response = filterProducts.filter((product) => product.sale === true);
     filterProducts = response;
-  };
+  }
 
   return filterProducts;
 };
