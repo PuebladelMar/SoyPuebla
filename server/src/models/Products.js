55d847d0d19a72e0e8a6ecc933102dec97d62fe2
Franco León Cáceres <110066828+FrancoCaceres1@users.noreply.github.com>
2023-08-03 17:37:32 -0300

    Update Products.js

--- server/src/models/Products.js
+++ server/src/models/Products.js
@@ -10,7 +10,7 @@ module.exports = (sequelize) => {
       },
       name: {
         type: DataTypes.STRING,
-        unique:true,
+        unique: true,
         allowNull: false,
         validate: {
           len: {
