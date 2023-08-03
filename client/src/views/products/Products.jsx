dd4016a2dcfe2779103f966e480f5aec428be3ab
MatuUrrutia <matu.urrutia.dt@gmail.com>
2023-08-03 15:44:44 -0400

    feat (client/products): Add products view - M.Urrutia

---
+++ client/src/views/products/Products.jsx
@@ -0,0 +1,11 @@
+import CardContainer from "../../componentes/cardContainer/CardContainer";
+
+function Products() {
+    return (
+      <div >
+        <CardContainer/>
+      </div>
+    );
+  }
+
+  export default Products;
