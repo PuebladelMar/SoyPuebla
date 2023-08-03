6b944840eaa9845728469864b920a38c82e24aa8
EzequielMenendez <ezemenendez2003@gmail.com>
2023-08-03 09:44:45 -0300

    fix(route-postSerie): updated route post serie/Ezequiel

--- server/src/controllers/Products/controllPostSerie.js
+++ server/src/controllers/Products/controllPostSerie.js
@@ -1,11 +1,9 @@
 const { Series } = require("../../db.js");
 
-const postNewSerie = async (serie) => {
-  const [newSerie, created] = await Series.findOrCreate({where: {serie}});
+const postNewSerie = async (name) => {
+  const [newSerie, created] = await Series.findOrCreate({where: {name}});
   if(!created)throw Error("size already exists");
   return newSerie;
 };
 
-module.exports = {
-  postNewSerie,
-};
+module.exports = postNewSerie
