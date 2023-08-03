2bd1c6064457c4f72196d3f6043705aeb01cbe03
Matu Urrutia <115486717+MatuUrrutia@users.noreply.github.com>
2023-08-03 01:05:22 -0400

    Enrutamiento (#62)
    * fix (client/home): New routing for card container - M.Urrutia
    
    * fix (client/home): New rute for series container - M. Urrutia
    
    * feat (client/home): Add reseñas route - M. Urrutia
    
    * feat (client/home): Add reseñas route and change order - M. Urrutia

--- client/src/views/home/Home.jsx
+++ client/src/views/home/Home.jsx
@@ -4,18 +4,22 @@ import Footer from "../../componentes/footer/Footer";
 import Slider from "../../componentes/slider/Slider";
 import Newsletter from "../../componentes/newsletter/newsletter";
 import SeriesContainer from '../../componentes/seriesContainer/SeriesContainer';
+import Reseñas from "../../componentes/reseñas/Reseñas"
 
 
 
 const Home = () => {
   return (
     <div>
+      
       <NavBar />
       <Slider />
-      <CardContainer />
-      <Newsletter />
       <SeriesContainer />
+      <CardContainer />
+      <Reseñas />
+      <Newsletter />
       <Footer />
+
     </div>
   );
 };
