0b067e5c61bc870d07e7b38a1d3ddccbc093e90b
CaritoCarvacho <flga.carolinacarvacho@gmail.com>
2023-08-03 00:30:50 -0300

    feat(client/componentes):add newsletter - Carito

--- client/src/views/home/Home.jsx
+++ client/src/views/home/Home.jsx
@@ -1,18 +1,17 @@
-
-import NavBar from '../../componentes/navbar/NavBar';
-import CardContainer from '../../componentes/cardContainer/CardContainer';
+import NavBar from "../../componentes/navbar/NavBar";
+import CardContainer from "../../componentes/cardContainer/CardContainer";
 import Footer from "../../componentes/footer/Footer";
-import Slider from '../../componentes/slider/Slider';
-
+import Slider from "../../componentes/slider/Slider";
+import Newsletter from "../../componentes/newsletter/newsletter";
 
 const Home = () => {
   return (
     <div>
       <NavBar />
-      <Slider/>
+      <Slider />
       <CardContainer />
+      <Newsletter />
       <Footer />
-
     </div>
   );
 };
