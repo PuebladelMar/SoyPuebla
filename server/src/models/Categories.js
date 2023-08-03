e2bb3ec20b8c35e5ce769c47c3cc06e93944dc6b
Matias Graner <matiasgraner@MacBook-Pro-de-Matias.local>
2023-08-03 17:35:59 -0300

    feat(src/models): add validations names/Matias

--- server/src/models/Categories.js
+++ server/src/models/Categories.js
@@ -11,12 +11,18 @@ sequelize.define(
       name: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: true,
         validate: {
           len: {
             args: [1, 20],
             msg: "Your field must have between 1 and 20 characters.",
           },
         },
+        set(value) {
+          const words = value.toLowerCase().split(' ');
+          const wordFixed = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+          this.setDataValue('name', wordFixed);
+        }
       },
     },
     { timestamps: false }
--- server/src/models/Users.js
+++ server/src/models/Users.js
@@ -1,6 +1,10 @@
 const { DataTypes, Sequelize } = require('sequelize');
 module.exports = (sequelize) => {
   sequelize.define('Users', {
-
+    // set(value) {
+    //   const words = value.toLowerCase().split(' ');
+    //   const wordFixed = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+    //   this.setDataValue('name', wordFixed);
+    // }
   }, {timestamps: false});
 };
