e54b71eaaf6ade07c22b46ac2f60f2618b8aedf5
Juan Cruz Mambretti <juancmam7@gmail.com>
2023-08-03 23:27:24 -0300

    feat(views/create): add create.jsx - J.Cruz

--- client/src/views/create/Create.jsx
+++ client/src/views/create/Create.jsx
@@ -20,11 +20,48 @@ const Create = () => {
         />
         <p>Emulamos error</p>
 
-        <label htmlFor='mainImage'>Main Image: </label>
+        <label htmlFor='mainImage'>Imagen Principal: </label>
         <textarea
           type='text'
           name='mainImage'
           placeholder='Main Image'
+          required
+        />
+
+        <label htmlFor='image'>Imagen: </label>
+        <input
+          type='json'
+          name='image'
+          required
+        />
+        <p>Emulamos error</p>
+
+        <label htmlFor='sale'>Oferta: </label>
+        <select
+          name='sale'
+          type='boolean'
+          placeholder='Oferta'
+        >
+          <option
+            value='def'
+            key='def'
+          >
+            False
+          </option>
+          <option
+            value='def'
+            key='def'
+          >
+            True
+          </option>
+        </select>
+        <p>Emulamos error</p>
+
+        <label htmlFor='description'>Descripcion: </label>
+        <textarea
+          type='text'
+          name='description'
+          placeholder='Descripcion'
           required
         />
       </form>
