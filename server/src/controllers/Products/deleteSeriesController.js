19ea17c8b8dd345600d8f76afb084e42fd5831d9
Ezequiel Menéndez <128303374+EzequielMenendez@users.noreply.github.com>
2023-08-04 19:41:58 -0300

    Update deleteSeriesController.js

--- server/src/controllers/Products/deleteSeriesController.js
+++ server/src/controllers/Products/deleteSeriesController.js
@@ -1,19 +1,14 @@
 const { Series } = require('../../db.js');
 
 const deleteSeriesController = async (id) => {
-  try {
-   
-    const series = await Series.findByPk(id);
-    if (!series) {
-      throw new Error("Serie not found.");
-    }
-
-    await series.destroy();
-
-    return { message: "Serie deleted successfully." };
-  } catch (error) {
-    throw error;
+  const series = await Series.findByPk(id);
+  if (!series) {
+    throw new Error("Serie not found.");
   }
+
+  await series.destroy();
+
+  return { message: "Serie deleted successfully." };
 };
 
 module.exports = deleteSeriesController;
