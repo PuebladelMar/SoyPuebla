adc81e53f73ce6c79dede0ea736aa6d24331ff44
Franco León Cáceres <110066828+FrancoCaceres1@users.noreply.github.com>
2023-08-04 19:42:30 -0300

    Update deleteProduct.js

--- server/src/handlers/Products/deleteProduct.js
+++ server/src/handlers/Products/deleteProduct.js
@@ -2,13 +2,8 @@ const deleteProductController = require('../../controllers/Products/deleteProduc
 
 const deleteProduct = async (req, res) => {
   const { id } = req.params;
-
-  try {
     const result = await deleteProductController(id);
     return res.status(200).json(result);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
 };
 
 module.exports = deleteProduct;
