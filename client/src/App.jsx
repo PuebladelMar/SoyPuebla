6c796c59804bfaf8907210f4842a9b3c7b77cb03
Matu Urrutia <115486717+MatuUrrutia@users.noreply.github.com>
2023-08-03 22:21:53 -0400

    App (#88)
    * fix (client/src): Add product endpoint on App - M.Urrutia
    
    * feat (client/src y home): Add new endpoints and routing - M.Urrutia

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,32 @@ import About from "./views/about/About";
 import Home from "./views/home/Home";
 import Landing from "./views/landing/Landing";
 import Products from "./views/products/Products";
+import NavBar from "./componentes/navbar/NavBar";
+import Footer from "./componentes/footer/Footer";
+import Cart from "./views/cart/Cart";
+import Pay from "./views/cart/Pay";
 import { Routes, Route } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  const { pathname } = useLocation();
+
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/products" element={<Products />} />
-    </Routes>
+    <div>
+      {pathname !== "/" && <NavBar />} {/* Renderizar NavBar en todas las rutas excepto "/" */}
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/pay" element={<Pay />} />
+
+      </Routes>
+      {pathname !== "/" && <Footer />} {/* Renderizar Footer en todas las rutas excepto "/" */}
+    </div>
   );
 }
 
 export default App;
+
--- client/src/views/home/Home.jsx
+++ client/src/views/home/Home.jsx
@@ -1,23 +1,21 @@
-import NavBar from "../../componentes/navbar/NavBar";
-import Footer from "../../componentes/footer/Footer";
 import Slider from "../../componentes/slider/Slider";
 import Newsletter from "../../componentes/newsletter/newsletter";
 import SeriesContainer from '../../componentes/seriesContainer/SeriesContainer';
 import Reseñas from "../../componentes/reseñas/Reseñas"
 
 
-
 const Home = () => {
   return (
     <div>
-      
-      <NavBar />
+    <br />
       <Slider />
+      <br />
       <SeriesContainer />
+      <br />
       <Reseñas />
+      <br />
       <Newsletter />
-      <Footer />
-
+   
     </div>
   );
 };
