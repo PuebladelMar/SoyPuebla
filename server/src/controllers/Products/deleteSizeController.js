38d322c740e50f2daa30d759edc4fcb9b6f97b2e
Franco León Cáceres <110066828+FrancoCaceres1@users.noreply.github.com>
2023-08-04 19:41:53 -0300

    Update deleteSizeController.js

--- server/src/controllers/Products/deleteSizeController.js
+++ server/src/controllers/Products/deleteSizeController.js
@@ -1,8 +1,6 @@
 const { Sizes } = require('../../db.js');
 
 const deleteSizeController = async (id) => {
-  try {
-   
     const Size = await Sizes.findByPk(id);
     if (!Size) {
       throw new Error("Size not found.");
@@ -11,9 +9,6 @@ const deleteSizeController = async (id) => {
     await Size.destroy();
 
     return { message: "Size deleted successfully." };
-  } catch (error) {
-    throw error;
-  }
 };
 
 module.exports = deleteSizeController;
