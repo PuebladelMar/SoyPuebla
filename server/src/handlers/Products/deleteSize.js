c1fd909f76e7e3ea1cb531c4e47720d59439af46
Franco León Cáceres <110066828+FrancoCaceres1@users.noreply.github.com>
2023-08-04 19:43:01 -0300

    Update deleteSize.js

--- server/src/handlers/Products/deleteSize.js
+++ server/src/handlers/Products/deleteSize.js
@@ -3,12 +3,8 @@ const deleteSizeController = require('../../controllers/Products/deleteSizeContr
 const deleteSize = async (req, res) => {
   const { id } = req.params;
 
-  try {
     const result = await deleteSizeController(id);
     return res.status(200).json(result);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
 };
 
 module.exports = deleteSize;
