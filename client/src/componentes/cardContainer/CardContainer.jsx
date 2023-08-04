e9f878693c4a86741967562fab2ff19ac5103665
Matu Urrutia <115486717+MatuUrrutia@users.noreply.github.com>
2023-08-04 16:22:32 -0400

    Redux (#108)
    * feat (client/redux): Add first global state and action - M.Urrutia
    
    * fix(redux):config redux fixed/Ezequiel
    
    * fix (redux/card/cardContainer): Add get products and render into card from DB - M.Urrutia
    
    ---------
    
    Co-authored-by: EzequielMenendez <ezemenendez2003@gmail.com>

--- client/src/componentes/cardContainer/CardContainer.jsx
+++ client/src/componentes/cardContainer/CardContainer.jsx
@@ -4,18 +4,18 @@ import Container from '@mui/material/Container';
 import Card from "../card/Card";
 import Grid from '@mui/material/Grid';
 
-function CardContainer() {
+function CardContainer(props) {
   return (
     <React.Fragment>
       <CssBaseline />
       <Container className="card-container" maxWidth="2560px" justifyContent="center" style={{ marginTop: '30px', paddingBottom: '30px', backgroundColor: '#f0f0f0' }}>
+   
         <Grid container spacing={3} justifyContent="center">
-
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((cardId) => (
-            <Grid item xs={12} md={6} lg={4} xl={3} key={cardId}>
-              <Card />
-            </Grid>
-          ))}
+        {props.products?.map((product) => (
+          <Grid item xs={12} md={6} lg={4} xl={3} key={product.id}>
+            <Card key={product.id} product={product}/>
+          </Grid>
+        ))}
         </Grid>
       </Container>
     </React.Fragment>
