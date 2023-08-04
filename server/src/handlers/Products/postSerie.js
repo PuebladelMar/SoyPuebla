49f8c5e400759369ee7517aae7f8e5cc109474c6
Matias Graner <matiasgraner@MacBook-Pro-de-Matias.local>
2023-08-04 16:03:13 -0300

    feat(/controllers/controllGetProducts && handlrers/postSeries): add filter image post controller y handlers and modelo series/Matias

--- server/src/handlers/Products/postSerie.js
+++ server/src/handlers/Products/postSerie.js
@@ -1,9 +1,9 @@
 const postNewSerie = require("../.././controllers/Products/controllPostSerie.js");
 
 const postSerie = async (req, res) => {
-  const { name } = req.body;
+  const { name, image  } = req.body;
   try {
-    const newSerie = await postNewSerie(name);
+    const newSerie = await postNewSerie(name , image );
     return res.status(201).json(newSerie);
   } catch (error) {
     res.status(400).json(error.message);
