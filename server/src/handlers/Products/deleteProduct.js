ae84f14892059905b7932df6d4018d01583f54f2
Ezequiel Menéndez <128303374+EzequielMenendez@users.noreply.github.com>
2023-08-04 19:44:35 -0300

    Update deleteProduct.js

--- server/src/handlers/Products/deleteProduct.js
+++ server/src/handlers/Products/deleteProduct.js
@@ -1,9 +1,13 @@
 const deleteProductController = require('../../controllers/Products/deleteProductController');
 
 const deleteProduct = async (req, res) => {
-  const { id } = req.params;
+  try{
+    const { id } = req.params;
     const result = await deleteProductController(id);
     return res.status(200).json(result);
+  }catch(error){
+    res.status(400).json({error: error.message});
+  };
 };
 
 module.exports = deleteProduct;
