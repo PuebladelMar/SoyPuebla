bc76ff4cc03e39e987ea55627cdcfcaf00c0bd2e
Ezequiel Menéndez <128303374+EzequielMenendez@users.noreply.github.com>
2023-08-04 19:40:53 -0300

    Update deleteCategoryController.js

--- server/src/controllers/Products/deleteCategoryController.js
+++ server/src/controllers/Products/deleteCategoryController.js
@@ -1,19 +1,14 @@
 const { Categories } = require('../../db.js');
 
 const deleteCategoryController = async (id) => {
-  try {
-   
-    const category = await Categories.findByPk(id);
-    if (!category) {
-      throw new Error("category not found.");
-    }
-
-    await category.destroy();
-
-    return { message: "category deleted successfully." };
-  } catch (error) {
-    throw error;
+  const category = await Categories.findByPk(id);
+  if (!category) {
+    throw new Error("category not found.");
   }
+
+  await category.destroy();
+
+  return { message: "category deleted successfully." };
 };
 
 module.exports = deleteCategoryController;
