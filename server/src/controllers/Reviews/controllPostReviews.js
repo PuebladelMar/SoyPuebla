14e1dc9ecd56817f10c69f61e9ac9b249a1646a6
Farith Romero <farithromero@gmail.com>
2023-08-04 18:13:38 -0500

    connect(controllers/reviews/PostReviews): new relations betwen intermadiate table

--- server/src/controllers/Reviews/controllPostReviews.js
+++ server/src/controllers/Reviews/controllPostReviews.js
@@ -1,8 +1,20 @@
-const { Reviews } = require('../../db');
+const { Reviews, ProductReviews } = require("../../db");
 
 const controllPostReviews = async (req) => {
-  const { score, title, userId, productId, description,  } = req.body;
-  const newReview = await Reviews.create({ score, title, userId, productId, description });
+  const { score, title, userId, productId, description } = req.body;
+
+  const newReview = await Reviews.create({
+    score,
+    title,
+    userId,
+    productId,
+    description,
+  });
+  const newProdutcReview = await ProductReviews.create({
+    ProductId: productId,
+    ReviewId: newReview.id,
+  });
+
   return newReview;
 };
 
--- server/src/handlers/Reviews/postReviews.js
+++ server/src/handlers/Reviews/postReviews.js
@@ -4,7 +4,7 @@ const postReview = async(req, res)=>{
     console.log(req)
     try{
         const newReview = await controllPostReviews(req);
-        res.status(201).json({message: newReview});
+        res.status(201).json([newReview]);
     }catch (error) {
         res.status(400).json({error: error.message});
     };
