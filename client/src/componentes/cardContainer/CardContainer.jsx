5a64a5565c33c533f363b2b6c4fad142120ebfc7
Matu Urrutia <115486717+MatuUrrutia@users.noreply.github.com>
2023-08-04 00:41:47 -0400

    Card container (#96)
    * feat (client/cardContainer): Create card container - M. Urrutia
    
    * fix (client/CardContainer): Responsive implementation for card container - M.Urrutia

--- client/src/componentes/cardContainer/CardContainer.jsx
+++ client/src/componentes/cardContainer/CardContainer.jsx
@@ -1,24 +1,26 @@
+
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
 import Card from "../card/Card";
-
-import "./CardContainer.css";
-
+import Grid from '@mui/material/Grid';
 
 function CardContainer() {
-  
-
   return (
-    <div className="card-list">
-      
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-      
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="2560px" style={{ border: '2px solid #333', backgroundColor: '#f0f0f0' }}>
+        <Grid container spacing={3}>
+
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((cardId) => (
+            <Grid item xs={12} md={6} lg={4} xl={3} key={cardId}>
+              <Card />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </React.Fragment>
   );
 }
-
 
 export default CardContainer;
