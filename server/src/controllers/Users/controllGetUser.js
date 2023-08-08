b7fcc078f276b53ff92cca2572027032334473d1
Franco León Cáceres <francocaceres008@gmail.com>
2023-08-07 22:24:32 -0300

    feat(Users-Routes): added post and get users route/Franco

--- server/src/controllers/Users/controllGetUser.js
+++ server/src/controllers/Users/controllGetUser.js
@@ -1,7 +1,8 @@
 const { Users } = require("../../db.js");
 
 const getAllUsers = async () => {
-  
+  const usersList = await Users.findAll();
+  return usersList;
 };
 
 module.exports = getAllUsers;
