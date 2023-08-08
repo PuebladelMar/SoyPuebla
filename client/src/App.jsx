0aa441156ea4ef4b2a9e684e6284e9acd2bcc0df
[danitkd] <[tkdanip77@gmail.com]>
2023-08-08 15:03:40 -0500

    (feat/integration-mercado-pago/Farith-Eze-MatiU): add payment-mp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,25 @@
-import './App.css';
-import About from './views/about/About';
-import Home from './views/home/Home';
-import Landing from './views/landing/Landing';
-import Products from './views/products/Products';
-import NavBar from './componentes/navbar/NavBar';
-import Footer from './componentes/footer/Footer';
-import Cart from './views/cart/Cart';
-import Pay from './views/cart/Pay';
-import Create from './views/create/Create';
-import Login from './views/login/Login';
-import Detail from './views/detail/Detail';
-import ErrorPage from './views/errorPage/ErrorPage'
-import { Routes, Route } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
+import "./App.css";
+import About from "./views/about/About";
+import Home from "./views/home/Home";
+import Landing from "./views/landing/Landing";
+import Products from "./views/products/Products";
+import NavBar from "./componentes/navbar/NavBar";
+import Footer from "./componentes/footer/Footer";
+import Cart from "./views/cart/Cart";
+import Pay from "./views/cart/Pay";
+import Create from "./views/create/Create";
+import Login from "./views/login/Login";
+import Detail from "./views/detail/Detail";
+import MercadoPago from "./views/mercadoPago/comprar/mercadopago";
+import EnProceso from './views/mercadoPago/EnProceso/enProceso'
+import FalloDeCompra from "./views/mercadoPago/FalloDeCompra/falloDeCompra";
+import ErrorPage from "./views/errorPage/ErrorPage";  
+import { Routes, Route } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { getProducts } from "./redux/Actions";
-import SearchBar from './componentes/searchBar/SearchBar';
+import SearchBar from "./componentes/searchBar/SearchBar";
 
 function App() {
   const { pathname } = useLocation();
@@ -26,54 +29,25 @@ function App() {
     dispatch(getProducts());
   }, [dispatch]);
 
-
   return (
     <div>
-
       {<NavBar />}
       <Routes>
-        <Route
-          path='/'
-          element={<Landing />}
-        />
-        <Route
-          path='/home'
-          element={<Home />}
-        />
-        <Route
-          path='/about'
-          element={<About />}
-        />
-        <Route
-          path='/products'
-          element={<Products />}
-        />
-        <Route
-          path='/cart'
-          element={<Cart />}
-        />
-        <Route
-          path='/pay'
-          element={<Pay />}
-        />
-        <Route
-          path='/create'
-          element={<Create />}
-        />
-        <Route
-          path='/login'
-          element={<Login />}
-        />
-        <Route
-          path='/products/:id'
-          element={<Detail />}
-        />
-          <Route
-          path='*'
-          element={<ErrorPage/>}
-        />
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/pagar" element={<MercadoPago />} />
+        <Route path="/procesando" element={<EnProceso />} />
+        <Route path="/falloDeCompra" element={<FalloDeCompra />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/pay" element={<Pay />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/products/:id" element={<Detail />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
-      {pathname !== '/' && <Footer />}
+      {pathname !== "/" && <Footer />}
     </div>
   );
 }
