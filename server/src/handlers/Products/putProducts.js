cd948c18f48d16ea11b5ee90be17cd8652c104a9
EzequielMenendez <ezemenendez2003@gmail.com>
2023-08-09 15:56:20 -0300

    feat(routes/putProduct):added put route-handler-controller Products/Ezequiel

---
+++ server/src/handlers/Products/putProducts.js
@@ -0,0 +1,12 @@
+const controllPutProduct = require("../../controllers/Products/controllPutProducts");
+
+const putProducts = async(req, res)=>{
+    try {
+        const response = await controllPutProduct(req);
+        res.status(200).json({message: response});
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    };
+};
+
+module.exports = putProducts;
