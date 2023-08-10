76a4b30953c084a74293000a73c943336685d741
CaritoCarvacho <flga.carolinacarvacho@gmail.com>
2023-08-10 18:34:37 -0300

    fix(client/components)fix footer - Carito)

--- client/src/componentes/footer/Footer.jsx
+++ client/src/componentes/footer/Footer.jsx
@@ -4,15 +4,15 @@ const Footer = () => {
   return (
     <footer>
       <div className="footer-container">
-        <p>© 2023 Puebla del Mar. Todos los derechos reservados.</p>
         <div className="footer-links">
-          <a href="/centro-ayuda">Centro de Ayuda</a>
-          <a href="/preguntas-frecuentes">Preguntas Frecuentes</a>
-          <a href="/about">Quienes somos</a>
-          <a href="/cambios-devoluciones">Cambios y Devoluciones</a>
-          <a href="/medios-pago">Medios de pago</a>
+          <a href="/centro-ayuda">Centro de Ayuda | </a>
+          <a href="/preguntas-frecuentes">Preguntas Frecuentes | </a>
+          <a href="/about">Quienes somos | </a>
+          <a href="/cambios-devoluciones">Cambios y Devoluciones | </a>
+          <a href="/medios-pago">Medios de pago | </a>
           <a href="/medios-envio">Medios de envío</a>
         </div>
+        <p>© 2023 Puebla del Mar. Todos los derechos reservados.</p>
       </div>
     </footer>
   );
