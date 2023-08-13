8cb377416768b2662316c0fd8e41fb17133a0292
EzequielMenendez <ezemenendez2003@gmail.com>
2023-08-12 21:13:28 -0300

    feat(route/getHistory): add getHistory handler-controller/Eze

--- server/src/controllers/History/controllPostHistory.js
+++ server/src/controllers/History/controllPostHistory.js
@@ -3,8 +3,6 @@ const { Carts, Histories, Stocks, Products } = require("../../db");
 const controllPostHistory = async(userId)=>{
     const userCart = await Carts.findAll({where: { UserId: userId }});
 
-    const stocksDetails = [];
-
     const insertPromises = userCart.map(async (user) => {
 
         const userStock = await Stocks.findByPk(user.StockId);
