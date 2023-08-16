4049d6dcad818507bcc42406610481f5b062e971
Matu Urrutia <115486717+MatuUrrutia@users.noreply.github.com>
2023-08-15 20:59:53 -0400

    Colorpicker (#247)
    * feat (client/createColor): Create color feature and color picker implementation - M.Urrutia
    
    * feat (client/createColor): Color creation and functionality created - M.Urrutia

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,7 +26,9 @@ import Shipment from "./views/shipment/shipment";
 import CreateCategory from "./views/create/createCategory/CreateCategory";
 import ReviewsForm from './componentes/reviews/ReviewsForm';
 import SizeChart from "./views/sizeChart/sizeChart";
+import CreateColor from "./views/create/createColor/createColor";
 import CreateSerie from "./views/create/createSerie/CreateSerie";
+
 axios.defaults.baseURL = "http://localhost:3001/";
 
 
@@ -69,6 +71,7 @@ function App() {
         <Route path="/history" element={<History />} />
         <Route path="/shipment" element={<Shipment />} />
         <Route path="/create-category" element={<CreateCategory />} />
+        <Route path="/create-color" element={<CreateColor />} />
         <Route path="/size-chart" element={<SizeChart />} />
         <Route path='/products/reviews' element={<ReviewsForm />} />
         <Route path="/create-serie" element={<CreateSerie />} />
