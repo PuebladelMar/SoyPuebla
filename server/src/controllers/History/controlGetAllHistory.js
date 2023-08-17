01935e1d5ec749ebd8029a9478e1440cbed17402
Matias <126165908+manmatia@users.noreply.github.com>
2023-08-17 18:56:37 -0300

    Update controlGetAllHistory.js

--- server/src/controllers/History/controlGetAllHistory.js
+++ server/src/controllers/History/controlGetAllHistory.js
@@ -1,14 +1,79 @@
-const { Histories, Stocks, Products, Sizes, Colors } = require("../../db");
+const { Histories, Stocks, Products, Sizes, Colors, Users, sequelize } = require("../../db");
+const { Op } = require("sequelize");
 
-const controllGetAllHistory = async (userId) => {
-  const userHistory = await Histories.findAll();
+const controllGetAllHistory = async () => {
+  try {
+    const userHistory = await Histories.findAll();
 
-  return userHistory
-  };
+    const stockIds = userHistory.map((user) => user.StockId);
 
-  
+    const stockList = await Stocks.findAll({
+      where: {
+        id: {
+          [Op.in]: stockIds,
+        },
+      },
+      attributes: ['ProductId', 'ColorId', 'SizeId'],
+    });
 
- 
+    const stockAttributes = await Promise.all(stockList.map(async (stock) => {
+      const product = await Products.findOne({
+        where: {
+          id: stock.ProductId,
+        },
+        attributes: ['name']
+      });
 
+      const color = await Colors.findOne({
+        where: {
+          id: stock.ColorId,
+        },
+        attributes: ['name'],
+      });
+
+      const size = await Sizes.findOne({
+        where: {
+          id: stock.SizeId,
+        },
+        attributes: ['name'],
+      });
+
+      return {
+        product: product ? product.name : '',
+        color: color ? color.name : '',
+        size: size ? size.name : '',
+      };
+    }));
+
+    const userIds = userHistory.map((user) => user.UserId);
+
+    const userAttributes = await Users.findAll({
+      where: {
+        id: {
+          [Op.in]: userIds,
+        },
+      },
+      attributes: ['id', 'emailAddress', 'userRole', 'banUser', 'fullName'],
+    });
+
+    const userMap = userAttributes.reduce((acc, user) => {
+      acc[user.id] = user.dataValues;
+      return acc;
+    }, {});
+
+    const result = userHistory.map((history, index) => ({
+      ...history.dataValues,
+      attributes: {
+        ...stockAttributes[index],
+        ...userMap[history.UserId],
+      },
+    }));
+
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
 module.exports = controllGetAllHistory;
