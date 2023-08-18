fb3a602825e4655964e8151a459280e7d26387b9
Juan Cruz Mambretti <juancmam7@gmail.com>
2023-08-18 16:19:10 -0300

    feat(admin/products): add - J.Cruz

--- client/src/views/productsInfo/allTalles/AllTalles.jsx
+++ client/src/views/productsInfo/allTalles/AllTalles.jsx
@@ -1,7 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getSizes } from '../../../redux/Actions';
 import './AllTalles.css';
 
 const AllTalles = () => {
-  return <h1 className='talles'>TALLES</h1>;
+  const sizes = useSelector((state) => state.sizesList);
+
+  const dispatch = useDispatch();
+
+  const [allSizes, setSizes] = useState({
+    sizes: [],
+  });
+
+  useEffect(() => {
+    dispatch(getSizes());
+  }, [dispatch, allSizes]);
+  console.log(sizes);
+  // console.log(series);
+
+  return (
+    <div
+      className='coleccion'
+      name='sizes'
+      value='name'
+    >
+      {sizes.map((el) => (
+        <p
+          key={el.id}
+          value={el.name}
+        >
+          {el.name}
+        </p>
+      ))}
+    </div>
+  );
 };
 
 export default AllTalles;
