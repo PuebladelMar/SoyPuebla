0b8cbe174690db57352fd82476a19b78bb240d16
Matias Graner <matiasgraner@MacBook-Pro-de-Matias.local>
2023-08-19 22:56:41 -0300

    views name dashboard

--- client/src/views/dashboard/listItems.jsx
+++ client/src/views/dashboard/listItems.jsx
@@ -18,18 +18,18 @@ export const mainListItems = (
       <ListItemIcon>
         <DashboardIcon />
       </ListItemIcon>
-      <ListItemText primary='Dashboard' />
+      <ListItemText primary='Informacion' />
       <NavLink to="/info">
-      <ListItemText primary="Informations" />
+      <ListItemText primary="" />
       </NavLink>
     </ListItemButton>
     <ListItemButton>
       <ListItemIcon>
         <ShoppingCartIcon />
       </ListItemIcon>
-      <ListItemText primary='Historial de compra' />
+      <ListItemText primary='Historial de compras' />
       <NavLink to="/dashboard/history">
-        <ListItemText primary="Historial de compras" />
+        <ListItemText primary="" />
       </NavLink>
     </ListItemButton>
     <ListItemButton>
