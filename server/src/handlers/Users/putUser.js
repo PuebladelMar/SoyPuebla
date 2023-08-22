5a2f2fd3de5c1f3b118ca1f64dde1d3d4397c624
Matias Graner <matiasgraner@MacBook-Pro-de-Matias.local>
2023-08-22 18:36:48 -0300

    feat(server/src/controllers/users/controller & handlers put): add route put users handlers and controllers/Matias Graneros

--- server/src/handlers/Users/putUser.js
+++ server/src/handlers/Users/putUser.js
@@ -2,15 +2,14 @@ const controllPutUser = require("../../controllers/Users/controllPutUser");
 
 const putUser = async (req, res) => {
   const { id } = req.params;
-  const { banUser, userRole } = req.body;
-
+  const { banUser, userRole } = req.body; 
   try {
     const updatedFields = {
-      banUser: banUser, 
-      userRole: userRole, 
+      banUser: banUser,
+      userRole: userRole,
     };
-    
-    const user = await controllPutUser(id, updatedFields);
+
+    const user = await controllPutUser(id, updatedFields); 
     return res.status(200).json(user);
   } catch (error) {
     return res.status(500).json({ error: error.message });
