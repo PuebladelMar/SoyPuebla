0d41febc7fcfaa688dd6c3782b2e4d59b0fb33b3
AgusNazer <agustin.nazer@hotmail.com>
2023-08-23 19:47:27 -0300

    feat(ban/ban message) : add view - A.Nazer

--- client/src/views/ban/BannedUserPage.jsx
+++ client/src/views/ban/BannedUserPage.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import "./BannedUserPage.css"
+import Swal from 'sweetalert2';
+import { useEffect } from 'react';
 
 function BannedUserPage() {
+
+  useEffect(() => {
+    Swal.fire({
+      title: 'Usuario bloqueado',
+      width: 600,
+      padding: '3em',
+      color: '#999999',
+      confirmButtonColor: '#517f7F',
+      backdrop: `
+      #999999
+        left top
+        no-repeat
+      `
+    })
+  }, []);
   return (
-    <div className='bann'>
-      <h1>Usuario Bloqueado</h1>
-      <p>Tu cuenta ha sido bloqueada y no tienes acceso a esta página.</p>
-      <p>si sos mati urrutia te bloqueamos porque no servis .</p>
+    <div className='banned-container'>
+      <div className='banned-content'>
+        <h2 className='banned-title '>Acceso Bloqueado</h2>
+        <p className='banned-text'>Tu cuenta ha sido bloqueada y no tienes acceso a esta página.</p>
+      </div>
     </div>
   );
 };
