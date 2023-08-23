c907fe62b7bc78eb5257b1ea332455abea122228
EzequielMenendez <ezemenendez2003@gmail.com>
2023-08-23 17:08:21 -0300

    fix(payState):fix mailError/Eze

--- client/src/views/mercadoPago/PayState.jsx
+++ client/src/views/mercadoPago/PayState.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { useDispatch, useSelector} from "react-redux";
-import { addHistory, deleteCartUser, getUserById, sendStatusPurchaseMail } from "../../redux/Actions";
+import { addHistory, deleteCartUser, getUserById, sendStatusPurchaseMail, getAllInformation } from "../../redux/Actions";
 import WhatsAppIcon from "../../assets/images/Whatsapp.png";
 import "./PayState.css";
 
@@ -24,7 +24,7 @@ function PayState() {
           await dispatch(addHistory(userId, "approved"));
           await dispatch(deleteCartUser(userId, true));
           await dispatch(getAllInformation());
-          const emailAddress =  userById.emailAddress;
+          const emailAddress = userById.user.emailAddress;
           let data = {
             emailsUsers: emailAddress,
             emailSubject: "Tu compra ha sido aprobada❤️🤗",
@@ -47,7 +47,7 @@ function PayState() {
         if (userId.length) {
           await dispatch(addHistory(userId, "rejected"));
           dispatch(getUserById(userId));
-          const emailAddress =  userById.emailAddress;
+          const emailAddress = userById.user.emailAddress;
           let data = {
             emailsUsers: emailAddress,
             emailSubject: "Tu compra ha sido rechazada💔",
