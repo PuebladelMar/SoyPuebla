464053b76dbadb8d7a9e8a65f9fe0ada4c100dc7
EzequielMenendez <ezemenendez2003@gmail.com>
2023-08-23 16:13:19 -0300

    fix(deletes): delete products without colo-category-serie/Eze

--- server/src/controllers/Products/controllDeleteColor.js
+++ server/src/controllers/Products/controllDeleteColor.js
@@ -1,20 +1,37 @@
-
-
-//! elmino por completo de la base de datos
-const { Colors } = require('../../db');
+const { Colors, Products, ColorImages } = require('../../db');
 
 const controllDeleteColor = async (id) => {
-  try {
-    const color = await Colors.findByPk(id);
-    if (!color) throw Error('Color no encontrado');
+  const color = await Colors.findByPk(id);
+  if (!color) throw Error('Color no encontrado');
+  const colorImagesWithColor = await ColorImages.findAll({
+    where: {
+      ColorId: color.id
+    }
+  });
 
-    await color.destroy();
+  const productIdsWithColor = colorImagesWithColor.map(image => image.ProductId);
 
-    return { message: 'Color eliminado exitosamente' };
-  } catch (error) {
-    console.error('Error en el controlador:', error);
-    throw error;
+  const productsWithColor = await Products.findAll({
+    where: {
+      id: productIdsWithColor
+    }
+  });
+
+  for (const product of productsWithColor) {
+    const remainingColorImages = await ColorImages.count({
+      where: {
+        ProductId: product.id
+      }
+    });
+
+    if (remainingColorImages === 1) {
+      await product.destroy();
+    }
   }
+
+  await color.destroy();
+
+  return { message: 'Color eliminado exitosamente' };
 };
 
 module.exports = controllDeleteColor;
