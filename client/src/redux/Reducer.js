eba23dc38e540ea2c8b8ceec5a3f1fa14c17196f
Franco León Cáceres <francocaceres008@gmail.com>
2023-08-23 13:57:18 -0300

    fixed bugs/Franco

--- client/src/redux/Reducer.js
+++ client/src/redux/Reducer.js
@@ -40,18 +40,14 @@ import {
   PUT_SIZES,
   PUT_COLECCIONS,
   ADD_HISTORY,
-
   GET_CATEGORIES_BY_NAME,
   GET_SERIES_BY_NAME,
   GET_COLORS_BY_NAME,
   GET_SIZES_BY_NAME,
-
   PUT_PRODUCTS,
   DELETE_PRODUCT,
   PUT_HISTORY_STATES,
-
-
-} from './ActionsTypes';
+} from "./ActionsTypes";
 
 let initialState = {
   allProducts: [],
@@ -311,20 +307,22 @@ function rootReducer(state = initialState, action) {
         ...state,
         allProducts: action.payload,
       };
-      case PUT_PRODUCTS:
+    case PUT_PRODUCTS:
       return {
         ...state,
-        allProducts: action.payload
+        allProducts: action.payload,
       };
-      case DELETE_PRODUCT:
-        return {
-          ...state,
-          allProducts: action.payload,
-      case PUT_HISTORY_STATES:
-        return {
-          ...state,
-          allHistory: action.payload
-        };
+    case DELETE_PRODUCT:
+      return {
+        ...state,
+        allProducts: action.payload,
+      };
+    case PUT_HISTORY_STATES:
+      return {
+        ...state,
+        allHistory: action.payload,
+      };
+
     default:
       return state;
   }
