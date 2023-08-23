af6bc897e25c1025aaded25c2a77c8f0bb5f020b
Matias Graner <matiasgraner@MacBook-Pro-de-Matias.local>
2023-08-23 16:31:05 -0300

    (feat change)

--- client/src/views/ban/BannedUserPage.jsx
+++ client/src/views/ban/BannedUserPage.jsx
@@ -6,6 +6,7 @@ function BannedUserPage() {
     <div className='bann'>
       <h1>Usuario Bloqueado</h1>
       <p>Tu cuenta ha sido bloqueada y no tienes acceso a esta página.</p>
+      <p>si sos mati urrutia te bloqueamos porque no servis .</p>
     </div>
   );
 };
